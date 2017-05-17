7c61e051e1e6bccea4d4e44884f696b4477c5a23
Allen Huang <seji.huang@envisioncn.com>
2017-05-17 10:00:22 +0800

    modify index page

--- src/main/java/com/hsj/controller/IndexController.java
+++ src/main/java/com/hsj/controller/IndexController.java
@@ -1,7 +1,16 @@
 package com.hsj.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * @author Allen on 2017/5/16
  */
+@Controller
 public class IndexController {
+    @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
+    public String indexPage() {
+        return "index";
+    }
 }
